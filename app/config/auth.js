df61dd04918b3fda4a6628fcf5643c49c925b65c
Chris Phillips <chris.is.phillips@gmail.com>
2016-02-29 11:12:54 +0700

    removed old facebook strategy

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,12 +2,6 @@
 
 module.exports = {
 	
-	'facebookAuth' : {
-        'clientID'      : process.env.FACEBOOK_KEY, // your App ID
-        'clientSecret'  : process.env.FACEBOOK_SECRET, // your App Secret
-        'callbackURL'   : process.env.APP_URL + "auth/facebook/callback",
-        "profileFields" : ["name", "email"]
-    },
     
     'twitterAuth' : {
         'consumerKey'       : process.env.TWITTER_KEY,
